d3c2daf54be532a2a37bffcb7ec876f41612f765
Gilsabo <gilsala_88@hotmail.com>
2023-11-30 14:42:20 +0100

    Render time and date of each event

--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
+import sportData from '../public/sportData.json';
+
 export default function Home() {
-  return <main></main>;
+  const sportEvents = sportData.data;
+
+  console.log(sportEvents);
+  return (
+    <main>
+      <div>
+        {sportEvents.map((sportevent) => {
+          return (
+            <section>
+              <div>{sportevent.dateVenue}</div>
+              <div>{sportevent.timeVenueUTC}</div>
+            </section>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
