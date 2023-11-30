c4008dddf922d05b3a9a17ea34b0334dbade6444
Gilsabo <gilsala_88@hotmail.com>
2023-11-30 18:33:46 +0100

    Move file to events folder for better structure

--- app/page.js
+++ app/page.js
@@ -1,21 +1,3 @@
-import sportData from '../public/sportData.json';
-
 export default function Home() {
-  const sportEvents = sportData.data;
-
-  console.log(sportEvents);
-  return (
-    <main>
-      <div>
-        {sportEvents.map((sportevent) => {
-          return (
-            <section>
-              <div>{sportevent.dateVenue}</div>
-              <div>{sportevent.timeVenueUTC}</div>
-            </section>
-          );
-        })}
-      </div>
-    </main>
-  );
+  return <div></div>;
 }
