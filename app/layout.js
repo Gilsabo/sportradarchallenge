cf55cc7b8ab27e171dee871aeb5f9df44f9254ee
Gilsabo <gilsala_88@hotmail.com>
2023-11-30 18:27:10 +0100

    Add Link in h1 to root

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <h1>AFC Champions League</h1>
+        <Link href="/">
+          <h1>AFC Champions League</h1>
+        </Link>
         <Link href="/events">Events</Link>
         <Link href="/add-event">Add Event</Link>
         {children}
