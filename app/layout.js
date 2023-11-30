0dc3b78887775dda40a71dc465be886e11526c05
Gilsabo <gilsala_88@hotmail.com>
2023-11-30 18:23:55 +0100

    Add links for navigation

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Gil Sala Bordallo',
@@ -10,6 +11,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <h1>AFC Champions League</h1>
+        <Link href="/events">Events</Link>
+        <Link href="/add-event">Add Event</Link>
         {children}
       </body>
     </html>
