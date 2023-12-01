c22c9d317e4879cfdc1aa3839c4080d29f873abb
Gilsabo <gilsala_88@hotmail.com>
2023-12-01 10:52:52 +0100

    Get each slug from each team from params

--- app/events/[eventName]/page.js
+++ app/events/[eventName]/page.js
@@ -1,4 +1,15 @@
+import sportData from '../../../public/sportData.json';
+
 export default function eventName(props) {
+  const sportEvents = sportData.data;
   console.log('params', props.params.eventName);
+  const slugsWithoutVs = props.params.eventName.replace('-vs-', ' ');
+  console.log('slugWithoutVs', slugsWithoutVs);
+  const splitSlugs = slugsWithoutVs.split(' ');
+  const homeTeamSlug = splitSlugs[0];
+  const awayTeamSlug = splitSlugs[1];
+
+  console.log('homeTeamSlug', homeTeamSlug, 'awayTeamSlug', awayTeamSlug);
+
   return <div>eventName</div>;
 }
