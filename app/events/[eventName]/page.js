7b4ccd6d7bb4bc6e5ea30f0a0298622a79fd3d0f
Gilsabo <gilsala_88@hotmail.com>
2023-12-05 06:57:45 +0100

    Move all file to EventNameInformation.js
    And then imported to Component EventNameInformation.js. In this way, we can carry out use client action

--- app/events/[eventName]/page.js
+++ app/events/[eventName]/page.js
@@ -1,38 +1,9 @@
-import sportData from '../../../public/sportData.json';
+import EventNameInformation from './EventNameInformation';
 
-export default function eventName(props) {
-  const sportEvents = sportData.data;
-  console.log('params', props.params.eventName);
-  const slugsWithoutVs = props.params.eventName.replace('-vs-', ' ');
-  console.log('slugWithoutVs', slugsWithoutVs);
-  const splitSlugs = slugsWithoutVs.split(' ');
-  const homeTeamSlug = splitSlugs[0];
-  const awayTeamSlug = splitSlugs[1];
-
-  console.log('homeTeamSlug', homeTeamSlug, 'awayTeamSlug', awayTeamSlug);
-  const findEventInformations = sportEvents.find(
-    (sportEvent) =>
-      sportEvent.homeTeam?.slug === homeTeamSlug ||
-      sportEvent.homeTeam === null,
-  );
-  console.log('findEventInformations', findEventInformations);
-
+export default function EventName(props) {
   return (
     <div>
-      <div>
-        <>
-          <div>Competition{findEventInformations.originCompetitionName}</div>
-          <div>{findEventInformations.stage.id}</div>
-          <div>
-            Result {findEventInformations.homeTeam?.officialName}{' '}
-            {findEventInformations.homeTeam?.teamCountryCode}{' '}
-            {findEventInformations.result?.homeGoals}{' '}
-            {findEventInformations.awayTeam?.officialName}{' '}
-            {findEventInformations.awayTeam?.teamCountryCode}{' '}
-            {findEventInformations.result?.awayGoals}
-          </div>
-        </>
-      </div>
+      <EventNameInformation params={props.params} />
     </div>
   );
 }
