6db36d5ce5ffbdb3f7a3fbfa7f3652191c904040
Gilsabo <gilsala_88@hotmail.com>
2023-12-05 12:47:36 +0100

    Add global colours and change font-weight

--- app/layout.js
+++ app/layout.js
@@ -10,15 +10,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-blue-900 text-white">
         <nav className="flex flex-col items-center mt-12">
           <Link href="/">
-            <h1 className="text-xl">AFC Champions League</h1>
+            <h1 className="font-extrabold text-3xl text-red-500">
+              AFC Champions League
+            </h1>
           </Link>
-          <Link className="mt-4" href="/events">
+          <Link className="mt-4 font-semibold" href="/events">
             Events
           </Link>
-          <Link className="mt-4" href="/add-event">
+          <Link className="mt-4 font-semibold" href="/add-event">
             Add Event
           </Link>
         </nav>
