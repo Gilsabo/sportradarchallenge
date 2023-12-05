b1d84f8a82de35178d41ef4bca142aab5fd22f15
Gilsabo <gilsala_88@hotmail.com>
2023-12-05 08:31:12 +0100

    Remove unnecessary div tag

--- app/events/page.js
+++ app/events/page.js
@@ -3,9 +3,7 @@ import EventsInformation from './EventsInformation';
 export default function Events() {
   return (
     <main>
-      <div>
-        <EventsInformation />
-      </div>
+      <EventsInformation />
     </main>
   );
 }
