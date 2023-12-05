9380d0f104666e33b4e8227bb1b4cbad9d551689
Gilsabo <gilsala_88@hotmail.com>
2023-12-05 08:31:44 +0100

    Center divs and add margins

--- app/events/EventsInformation.js
+++ app/events/EventsInformation.js
@@ -8,59 +8,61 @@ export default function EventsInformation() {
   console.log('sporteeventsss', sportEvents);
 
   return (
-    <main>
-      <div>
-        {sportEvents.map((sportevent) =>
-          sportevent.homeTeam && sportevent.awayTeam ? (
-            <section
-              key={`sportevent-div-${sportevent.dateVenue}-${
-                sportevent.homeTeam?.slug || 'to-be-confirmed'
-              }-vs-${sportevent.awayTeam?.slug || 'to-be-confirmed'}`}
+    <div className="flex flex-col items-center mt-8 text-center">
+      {sportEvents.map((sportevent) =>
+        sportevent.homeTeam && sportevent.awayTeam ? (
+          <section
+            className="mb-4"
+            key={`sportevent-div-${sportevent.dateVenue}-${
+              sportevent.homeTeam?.slug || 'to-be-confirmed'
+            }-vs-${sportevent.awayTeam?.slug || 'to-be-confirmed'}`}
+          >
+            <Link
+              href={`events/${sportevent.homeTeam.slug}-vs-${sportevent.awayTeam.slug}`}
             >
-              <Link
-                href={`events/${sportevent.homeTeam.slug}-vs-${sportevent.awayTeam.slug}`}
-              >
-                <div>{sportevent.dateVenue}</div>
-                <div>{sportevent.timeVenueUTC}</div>
-                <div>{sportevent.homeTeam.officialName}</div>
-                <div>{sportevent.awayTeam.officialName}</div>
-              </Link>
-            </section>
-          ) : (
-            <section
-              key={`sportevent-div-${
-                sportevent.homeTeam ? sportevent.homeTeam.officialName : ''
+              <div>{sportevent.dateVenue}</div>
+              <div>{sportevent.timeVenueUTC}</div>
+              <div>{sportevent.homeTeam.officialName}</div>
+              <span>vs.</span>
+              <div>{sportevent.awayTeam.officialName}</div>
+            </Link>
+          </section>
+        ) : (
+          <section
+            className="mb-4"
+            key={`sportevent-div-${
+              sportevent.homeTeam ? sportevent.homeTeam.officialName : ''
+            }-vs-${
+              sportevent.awayTeam ? sportevent.awayTeam.officialName : ''
+            }`}
+          >
+            <Link
+              href={`events/${
+                sportevent.homeTeam
+                  ? sportevent.homeTeam.slug
+                  : 'to-be-confirmed'
               }-vs-${
-                sportevent.awayTeam ? sportevent.awayTeam.officialName : ''
+                sportevent.awayTeam
+                  ? sportevent.awayTeam.slug
+                  : 'to-be-confirmed'
               }`}
             >
-              <Link
-                href={`events/${
-                  sportevent.homeTeam
-                    ? sportevent.homeTeam.slug
-                    : 'to-be-confirmed'
-                }-vs-${
-                  sportevent.awayTeam
-                    ? sportevent.awayTeam.slug
-                    : 'to-be-confirmed'
-                }`}
-              >
-                <div>{sportevent.dateVenue}</div>
-                {sportevent.homeTeam !== null ? (
-                  <div>{sportevent.homeTeam.officialName}</div>
-                ) : (
-                  <div>to be confirmed</div>
-                )}
-                {sportevent.awayTeam !== null ? (
-                  <div>{sportevent.awayTeam.officialName}</div>
-                ) : (
-                  <div>to be confirmed</div>
-                )}
-              </Link>
-            </section>
-          ),
-        )}
-      </div>
-    </main>
+              <div>{sportevent.dateVenue}</div>
+              {sportevent.homeTeam !== null ? (
+                <div>{sportevent.homeTeam.officialName}</div>
+              ) : (
+                <div>to be confirmed</div>
+              )}
+              <span>vs.</span>
+              {sportevent.awayTeam !== null ? (
+                <div>{sportevent.awayTeam.officialName}</div>
+              ) : (
+                <div>to be confirmed</div>
+              )}
+            </Link>
+          </section>
+        ),
+      )}
+    </div>
   );
 }
