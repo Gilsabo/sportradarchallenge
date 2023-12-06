12f4d2eb80b1d2a2396177e73ba4c0599bdc7752
Gilsabo <gilsala_88@hotmail.com>
2023-12-06 19:24:35 +0100

    Create handle error when info no available

---
+++ util/NotUpdated.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function NotUpdated() {
+  return (
+    <main className="flex justify-center mt-36 text-2xl text-center">
+      This information is not available yet
+    </main>
+  );
+}
