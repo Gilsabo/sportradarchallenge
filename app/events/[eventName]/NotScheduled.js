9ffb359e647d285654ffcf52ad5e0d2662271f0a
Gilsabo <gilsala_88@hotmail.com>
2023-12-06 17:01:20 +0100

    Change semantic tag

--- app/events/[eventName]/NotScheduled.js
+++ app/events/[eventName]/NotScheduled.js
@@ -2,8 +2,8 @@
 
 export default function NotScheduled() {
   return (
-    <div className="flex justify-center mt-36 text-2xl text-center">
+    <main className="flex justify-center mt-36 text-2xl text-center">
       No event scheduled yet
-    </div>
+    </main>
   );
 }
