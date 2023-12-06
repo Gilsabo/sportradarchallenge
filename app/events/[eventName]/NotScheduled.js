ea45d329a5d89d5aff98aa0456aaa06f9713af08
Gilsabo <gilsala_88@hotmail.com>
2023-12-06 16:22:06 +0100

    Add text-center

--- app/events/[eventName]/NotScheduled.js
+++ app/events/[eventName]/NotScheduled.js
@@ -2,7 +2,7 @@
 
 export default function NotScheduled() {
   return (
-    <div className="flex justify-center mt-36 text-2xl">
+    <div className="flex justify-center mt-36 text-2xl text-center">
       No event scheduled yet
     </div>
   );
