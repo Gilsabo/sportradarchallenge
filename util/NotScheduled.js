9feb77a156b649883497d390b38880d95bc4547d
Gilsabo <gilsala_88@hotmail.com>
2023-12-06 19:24:52 +0100

    Move file for better organization

---
+++ util/NotScheduled.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function NotScheduled() {
+  return (
+    <main className="flex justify-center mt-36 text-2xl text-center">
+      No event scheduled yet
+    </main>
+  );
+}
