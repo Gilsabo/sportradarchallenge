af33b0e2b9fb59aec1bc7248470a7ab5e9e13055
Gilsabo <gilsala_88@hotmail.com>
2023-12-14 19:27:57 +0100

    Center text

--- app/events/[eventName]/page.js
+++ app/events/[eventName]/page.js
@@ -2,7 +2,7 @@ import EventNameInformation from './EventNameInformation';
 
 export default function EventName(props) {
   return (
-    <main>
+    <main className="flex justify-center">
       <EventNameInformation params={props.params} />
     </main>
   );
